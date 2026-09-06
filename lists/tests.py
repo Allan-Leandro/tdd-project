64794f5b599d696add911048939c5a9bf8cc3f14
Allan <allanleandro312@gmail.com>
2026-09-06 17:17:20 -0300

    Persistindo os Dados

--- lists/tests.py
+++ lists/tests.py
@@ -16,3 +16,22 @@ class HomePageTest(TestCase):
         self.assertEqual(Item.objects.count(), 1)
         new_item = Item.objects.first()
         self.assertEqual(new_item.text, 'A new list item')
+
+class ItemModelTest(TestCase):
+
+    def test_saving_and_retrieving_items(self):
+        first_item = Item()
+        first_item.text = 'O primeiro item'
+        first_item.save()
+
+        second_item = Item()
+        second_item.text = 'O segundo item'
+        second_item.save()
+
+        saved_items = Item.objects.all()
+        self.assertEqual(saved_items.count(), 2)
+
+        first_saved_item = saved_items[0]
+        second_saved_item = saved_items[1]
+        self.assertEqual(first_saved_item.text, 'O primeiro item')
+        self.assertEqual(second_saved_item.text, 'O segundo item')

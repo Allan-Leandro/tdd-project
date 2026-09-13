77948af8a073ce7630e0a18186027a80c2a3d8d4
Allan <allanleandro312@gmail.com>
2026-09-13 13:26:34 -0300

    Implementando o Novo Projeto: Ajustando o Modelo

--- lists/tests.py
+++ lists/tests.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-from lists.models import Item
+from lists.models import Item, List
 
 class HomePageTest(TestCase):
 
@@ -11,16 +11,24 @@ class HomePageTest(TestCase):
         self.client.get('/')
         self.assertEqual(Item.objects.count(), 0)
 
-class ItemModelTest(TestCase):
+class ListAndItemModelsTest(TestCase):
 
     def test_saving_and_retrieving_items(self):
+        list_ = List()
+        list_.save()
+
         first_item = Item()
         first_item.text = 'O primeiro item'
+        first_item.list = list_
         first_item.save()
 
         second_item = Item()
         second_item.text = 'O segundo item'
+        second_item.list = list_
         second_item.save()
+
+        saved_list = List.objects.first()
+        self.assertEqual(saved_list, list_)
 
         saved_items = Item.objects.all()
         self.assertEqual(saved_items.count(), 2)
@@ -28,7 +36,9 @@ class ItemModelTest(TestCase):
         first_saved_item = saved_items[0]
         second_saved_item = saved_items[1]
         self.assertEqual(first_saved_item.text, 'O primeiro item')
+        self.assertEqual(first_saved_item.list, list_)
         self.assertEqual(second_saved_item.text, 'O segundo item')
+        self.assertEqual(second_saved_item.list, list_)
 
 class NewListTest(TestCase):
 
